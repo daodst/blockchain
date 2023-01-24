0affdfbd8d0bf1a8daa9beb9366304bca2872873
Danilo Fonseca <108163235+jolunna@users.noreply.github.com>
2023-01-24 22:07:44 +0800

    forks

--- app/forks.go
+++ app/forks.go
@@ -1,46 +1,46 @@
 package app
 
 import (
-	"fmt"
+    "fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
-	"github.com/tharsis/evmos/v4/types"
+    "github.com/evmos/evmos/v9/types"
 )
 
 // ScheduleForkUpgrade executes any necessary fork logic for based upon the current
 // block height and chain ID (mainnet or testnet). It sets an upgrade plan once
 // the chain reaches the pre-defined upgrade height.
-//
+
 // CONTRACT: for this logic to work properly it is required to:
-//
+
 // 	1) Release a non-breaking patch version so that the chain can set the scheduled upgrade plan at upgrade-height.
 // 	2) Release the software defined in the upgrade-info
 func (app *Evmos) ScheduleForkUpgrade(ctx sdk.Context) {
-	// NOTE: there are no testnet forks for the existing versions
-	if !types.IsMainnet(ctx.ChainID()) {
-		return
-	}
+    // NOTE: there are no testnet forks for the existing versions
+    if !types.IsMainnet(ctx.ChainID()) {
+        return
+    }
 
-	upgradePlan := upgradetypes.Plan{
-		Height: ctx.BlockHeight(),
-	}
+    upgradePlan := upgradetypes.Plan{
+        Height: ctx.BlockHeight(),
+    }
 
-	// handle mainnet forks
-	switch ctx.BlockHeight() {
+    // handle mainnet forks
+    switch ctx.BlockHeight() {
 
-	default:
-		// No-op
-		return
-	}
+    default:
+        // No-op
+        return
+    }
 
-	if err := app.UpgradeKeeper.ScheduleUpgrade(ctx, upgradePlan); err != nil {
-		panic(
-			fmt.Errorf(
-				"failed to schedule upgrade %s during BeginBlock at height %d: %w",
-				upgradePlan.Name, ctx.BlockHeight(), err,
-			),
-		)
-	}
+    if err := app.UpgradeKeeper.ScheduleUpgrade(ctx, upgradePlan); err != nil {
+        panic(
+            fmt.Errorf(
+                "failed to schedule upgrade %s during BeginBlock at height %d: %w",
+                upgradePlan.Name, ctx.BlockHeight(), err,
+            ),
+        )
+    }
 }
