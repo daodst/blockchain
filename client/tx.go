152a87f695aca671b9cb1cb19c1adc5874322e7d
Danilo Fonseca <108163235+jolunna@users.noreply.github.com>
2023-02-07 15:18:37 +0800

    tx client

--- client/tx.go
+++ client/tx.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"freemasonry.cc/blockchain/cmd/config"
 	"freemasonry.cc/blockchain/core"
 	"freemasonry.cc/blockchain/util"
@@ -21,11 +22,18 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
 	xauthsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/cosmos/cosmos-sdk/x/distribution/client/common"
+	distributionTypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govTypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	slashingTypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	stakeTypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	stakingTypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	evmhd "github.com/evmos/ethermint/crypto/hd"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	ttypes "github.com/tendermint/tendermint/types"
-	evmhd "github.com/tharsis/ethermint/crypto/hd"
 	"regexp"
+	"strings"
+	"time"
 )
 
 type TxClient struct {
@@ -70,7 +78,7 @@ func (this *TxClient) FindByByte(txhash []byte) (resultTx *ctypes.ResultTx, notF
 	}
 	resultTx, err = node.Tx(context.Background(), txhash, true)
 	if err != nil {
-		//
+		
 		notFound = this.isTxNotFoundError(err.Error())
 		if notFound {
 			err = nil
@@ -116,19 +124,19 @@ func (this *TxClient) isTxNotFoundError(errContent string) (ok bool) {
 */
 func (this *TxClient) SignAndSendMsg(address string, privateKey string, fee legacytx.StdFee, memo string, msg ...sdk.Msg) (txRes *core.BaseResponse, err error) {
 	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
-	//
+	
 	seqDetail, err := this.FindAccountNumberSeq(address)
 	if err != nil {
 		return
 	}
 
-	//
+	
 	signedTx, err := this.SignTx(privateKey, seqDetail, fee, memo, msg...)
 	if err != nil {
 		return
 	}
 
-	//，
+	
 	signPubkey, err := signedTx.GetPubKeys()
 	if err != nil {
 		return
@@ -141,18 +149,64 @@ func (this *TxClient) SignAndSendMsg(address string, privateKey string, fee lega
 		return nil, errors.New("sign error")
 	}
 
-	//
+	
 	signedTxBytes, err := this.SignTx2Bytes(signedTx)
 	if err != nil {
 		log.WithError(err).Error("SignTx2Bytes")
 		return
 	}
-	//
+	
 	txRes, err = this.Send(signedTxBytes)
 	if txRes != nil {
 		broadcastTxResponse := txRes.Data.(core.BroadcastTxResponse)
 		broadcastTxResponse.SignedTxStr = hex.EncodeToString(signedTxBytes)
 		txRes.Data = broadcastTxResponse
+	}
+	return
+}
+
+/**
+
+*/
+func (this *TxClient) SignAndSendMsg2(address string, privateKey string, fee legacytx.StdFee, memo string, msg ...sdk.Msg) (txRes *core.BaseResponse, err error) {
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
+	
+	seqDetail, err := this.FindAccountNumberSeq(address)
+	if err != nil {
+		return
+	}
+
+	
+	signedTx, err := this.SignTx(privateKey, seqDetail, fee, memo, msg...)
+	if err != nil {
+		return
+	}
+
+	
+	signPubkey, err := signedTx.GetPubKeys()
+	if err != nil {
+		return
+	}
+
+	signV2, _ := signedTx.GetSignaturesV2()
+	senderAddrBytes := signV2[0].PubKey.Address().Bytes()
+	signAddrBytes := signPubkey[0].Address().Bytes()
+	if !bytes.Equal(signAddrBytes, senderAddrBytes) {
+		return nil, errors.New("sign error")
+	}
+
+	
+	signedTxBytes, err := this.SignTx2Bytes(signedTx)
+	if err != nil {
+		log.WithError(err).Error("SignTx2Bytes")
+		return
+	}
+	
+	txRes, err = this.Send(signedTxBytes)
+	if txRes != nil {
+		//broadcastTxResponse := txRes.Data.(core.BroadcastTxResponse)
+		//broadcastTxResponse.SignedTxStr = hex.EncodeToString(signedTxBytes)
+		//txRes.Data = broadcastTxResponse
 	}
 	return
 }
@@ -185,7 +239,7 @@ func (this *TxClient) FindAccountNumberSeq(accountAddr string) (core.AccountNumb
 	return seq, nil
 }
 
-//
+
 func (this *TxClient) Send(req []byte) (txRes *core.BaseResponse, err error) {
 	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
 	response, err := PostRequest(this.ServerUrl, "/chat/tx/broadcast", req)
@@ -202,9 +256,9 @@ func (this *TxClient) Send(req []byte) (txRes *core.BaseResponse, err error) {
 	return
 }
 
-//
+
 func (this TxClient) GasMsg(msgType, msgs string) (sdk.Msg, error) {
-	log := util.BuildLog(util.GetStructFuncName(this), util.LmChainClient)
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
 	msgByte := []byte(msgs)
 	switch msgType {
 	case "cosmos-sdk/MsgSend":
@@ -247,14 +301,6 @@ func (this TxClient) GasMsg(msgType, msgs string) (sdk.Msg, error) {
 			return nil, err
 		}
 		return &msg, nil
-	case "chat/MsgSetChatFee":
-		msg := chatTypes.MsgSetChatFee{}
-		err := util.Json.Unmarshal(msgByte, &msg)
-		if err != nil {
-			log.WithError(err).Error("gasMsg.Unmarshal MsgSetChatFee")
-			return nil, err
-		}
-		return &msg, nil
 	case "chat/MsgSendGift":
 		msg := chatTypes.MsgSendGift{}
 		err := util.Json.Unmarshal(msgByte, &msg)
@@ -263,11 +309,11 @@ func (this TxClient) GasMsg(msgType, msgs string) (sdk.Msg, error) {
 			return nil, err
 		}
 		return &msg, nil
-	case "chat/MsgAddressBookSave":
-		msg := chatTypes.MsgAddressBookSave{}
+	case "chat/MsgChatSendGift":
+		msg := chatTypes.MsgChatSendGift{}
 		err := util.Json.Unmarshal(msgByte, &msg)
 		if err != nil {
-			log.WithError(err).Error("gasMsg.Unmarshal MsgAddressBookSave")
+			log.WithError(err).Error("gasMsg.Unmarshal MsgChatSendGift")
 			return nil, err
 		}
 		return &msg, nil
@@ -279,14 +325,6 @@ func (this TxClient) GasMsg(msgType, msgs string) (sdk.Msg, error) {
 			return nil, err
 		}
 		return &msg, nil
-	case "chat/MsgChangeGateway":
-		msg := chatTypes.MsgChangeGateway{}
-		err := util.Json.Unmarshal(msgByte, &msg)
-		if err != nil {
-			log.WithError(err).Error("gasMsg.Unmarshal MsgChangeGateway")
-			return nil, err
-		}
-		return &msg, nil
 	case "chat/MsgBurnGetMobile":
 		msg := chatTypes.MsgBurnGetMobile{}
 		err := util.Json.Unmarshal(msgByte, &msg)
@@ -295,11 +333,52 @@ func (this TxClient) GasMsg(msgType, msgs string) (sdk.Msg, error) {
 			return nil, err
 		}
 		return &msg, nil
+	case "chat/MsgSetChatInfo":
+		msg := chatTypes.MsgSetChatInfo{}
+		err := util.Json.Unmarshal(msgByte, &msg)
+		if err != nil {
+			log.WithError(err).Error("gasMsg.Unmarshal MsgSetChatInfo")
+			return nil, err
+		}
+		return &msg, nil
+	case "cosmos-sdk/MsgDelegate":
+		msg := stakeTypes.MsgDelegate{}
+		err := util.Json.Unmarshal(msgByte, &msg)
+		if err != nil {
+			log.WithError(err).Error("gasMsg.Unmarshal MsgDelegate")
+			return &msg, nil
+		}
+		return &msg, nil
+	case "cosmos-sdk/MsgUndelegate":
+		msg := stakeTypes.MsgUndelegate{}
+		err := util.Json.Unmarshal(msgByte, &msg)
+		if err != nil {
+			log.WithError(err).Error("gasMsg.Unmarshal MsgUndelegate")
+			return &msg, nil
+		}
+		return &msg, nil
+	case "cosmos-sdk/MsgWithdrawDelegationReward":
+		msg := distributionTypes.MsgWithdrawDelegatorReward{}
+		err := util.Json.Unmarshal(msgByte, &msg)
+		if err != nil {
+			log.WithError(err).Error("gasMsg.Unmarshal MsgWithdrawDelegationReward")
+			return &msg, nil
+		}
+		return &msg, nil
+	case "cosmos-sdk/MsgVote":
+		msg := govTypes.MsgVote{}
+		err := util.Json.Unmarshal(msgByte, &msg)
+		if err != nil {
+			log.WithError(err).Error("gasMsg.Unmarshal MsgVote")
+			return &msg, nil
+		}
+		return &msg, nil
 	}
+
 	return nil, nil
 }
 
-//
+
 func (this *TxClient) GasInfo(seqDetail core.AccountNumberSeqResponse, msg ...sdk.Msg) (sdk.Coin, uint64, sdk.DecCoin, error) {
 	log := core.BuildLog(core.GetFuncName(), core.LmChainClient)
 	fee := sdk.NewCoin(config.BaseDenom, sdk.ZeroInt())
@@ -328,10 +407,10 @@ func (this *TxClient) GasInfo(seqDetail core.AccountNumberSeqResponse, msg ...sd
 	}
 	gasPrice = gasPriceDec[0]
 	gasDec := sdk.NewDec(int64(gas))
-	//
+	
 	fee = sdk.NewCoin(config.BaseDenom, gasPrice.Amount.Mul(gasDec).TruncateInt())
 	if fee.IsLT(minFee) {
-		//,
+		
 		return minFee, gas, gasPrice, nil
 	}
 	return fee, gas, gasPrice, nil
@@ -339,7 +418,7 @@ func (this *TxClient) GasInfo(seqDetail core.AccountNumberSeqResponse, msg ...sd
 
 //gas
 func (this *TxClient) QueryGasPrice() (gasPrice sdk.DecCoins, err error) {
-	log := util.BuildLog(util.GetStructFuncName(this), util.LmChainClient)
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
 	resBytes, _, err := clientCtx.QueryWithData("custom/comm/"+types.QueryGasPrice, nil)
 	if err != nil {
 		log.WithError(err).Error("QueryWithData")
@@ -392,8 +471,8 @@ func (this *TxClient) SignTx(privateKey string, seqDetail core.AccountNumberSeqR
 		return nil, err
 	}
 	txBuild.SetGasLimit(fee.Gas)     //gas
-	txBuild.SetFeeAmount(fee.Amount) //
-	txBuild.SetMemo(memo)            //
+	txBuild.SetFeeAmount(fee.Amount) 
+	txBuild.SetMemo(memo)            
 	sigData := signing.SingleSignatureData{
 		SignMode:  signMode,
 		Signature: nil,
@@ -403,7 +482,7 @@ func (this *TxClient) SignTx(privateKey string, seqDetail core.AccountNumberSeqR
 		Data:     &sigData,
 		Sequence: seqDetail.Sequence,
 	}
-	//
+	
 	if err := txBuild.SetSignatures(sig); err != nil {
 		log.WithError(err).Error("SetSignatures")
 		return nil, err
@@ -439,7 +518,7 @@ func (this *TxClient) RegisterValidator(bech32DelegatorAddr, bech32ValidatorAddr
 		return
 	}
 
-	//
+	
 	err = commission.Validate()
 	if err != nil {
 		log.WithError(err).Error("commission.Validate")
@@ -455,8 +534,291 @@ func (this *TxClient) RegisterValidator(bech32DelegatorAddr, bech32ValidatorAddr
 		return
 	}
 	if resp.Status == 1 {
-		return resp, nil //
+		return resp, nil 
 	} else {
-		return resp, errors.New(resp.Info) //
+		return resp, errors.New(resp.Info) 
 	}
+}
+
+func (this *TxClient) QueryProposer(proposerId uint64) (resp *govTypes.Proposal, err error) {
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
+	params := govTypes.QueryProposalParams{ProposalID: proposerId}
+	bz, err := clientCtx.LegacyAmino.MarshalJSON(params)
+	if err != nil {
+		log.WithError(err).Error("MarshalJSON")
+		return
+	}
+	resBytes, _, err := clientCtx.QueryWithData("custom/gov/"+govTypes.QueryProposal, bz)
+	if err != nil {
+		log.WithError(err).Error("QueryWithData")
+		return
+	}
+	var res govTypes.Proposal
+	if resBytes != nil {
+		err = clientCtx.LegacyAmino.UnmarshalJSON(resBytes, &res)
+		if err != nil {
+			return
+		}
+		resp = &res
+	}
+	return
+}
+
+
+func (this *TxClient) UnjailValidator(bech32DelegatorAddr, bech32ValidatorAddr, privateKey string) (resp *core.BaseResponse, err error) {
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
+	validatorAddr, err := sdk.ValAddressFromBech32(bech32ValidatorAddr)
+	if err != nil {
+		log.WithError(err).Error("ValAddressFromBech32")
+		return
+	}
+	
+	validatorInfo, err := this.FindValidatorByValAddress(bech32ValidatorAddr)
+	if err != nil {
+		log.WithError(err).Error("FindValidatorByValAddress")
+		return
+	}
+	if !validatorInfo.Jailed {
+		return
+	}
+
+	accAddr, err := sdk.AccAddressFromBech32(bech32DelegatorAddr)
+	if err != nil {
+		log.WithError(err).Error("AccAddressFromBech32")
+		return
+	}
+	OperatorAddress := sdk.ValAddress(accAddr).String()
+	if validatorInfo.OperatorAddress != OperatorAddress {
+		return
+	}
+	
+	delegatorResponse, notFound, err := this.FindDelegation(bech32DelegatorAddr, bech32ValidatorAddr)
+	if err != nil {
+		if notFound {
+		} else {
+		}
+		return
+	}
+	
+	if delegatorResponse.Delegation.Shares.IsZero() {
+		return
+	}
+
+	tokens := validatorInfo.TokensFromShares(delegatorResponse.Delegation.Shares).TruncateInt()
+	if tokens.LT(validatorInfo.MinSelfDelegation) {
+		return
+	}
+
+	
+	msg := slashingTypes.NewMsgUnjail(validatorAddr)
+	fee := legacytx.NewStdFee(flags.DefaultGasLimit, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(1))))
+	resp, err = this.SignAndSendMsg2(bech32DelegatorAddr, privateKey, fee, "", msg)
+	if err != nil {
+		return
+	}
+	if resp.Status == 1 {
+		return resp, nil 
+	} else {
+		return resp, errors.New(resp.Info) 
+	}
+	return nil, nil
+}
+
+/**
+ 
+*/
+func (this *TxClient) FindValidatorByValAddress(bech32ValidatorAddr string) (validator *stakingTypes.Validator, err error) {
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
+	validatorAddr, err := sdk.ValAddressFromBech32(bech32ValidatorAddr)
+	if err != nil {
+		log.WithError(err).Error("ValAddressFromBech32")
+		err = errors.New(core.ParseAccountError)
+		return
+	}
+	params := stakingTypes.QueryValidatorParams{ValidatorAddr: validatorAddr}
+	bz, err := clientCtx.LegacyAmino.MarshalJSON(params)
+	if err != nil {
+		log.WithError(err).Error("MarshalJSON")
+		return nil, err
+	}
+	resBytes, _, err := clientCtx.QueryWithData("custom/staking/"+stakingTypes.QueryValidator, bz)
+	if err != nil {
+		log.WithError(err).Error("QueryWithData")
+		return nil, err
+	}
+	validator = &stakingTypes.Validator{}
+
+	err = clientCtx.LegacyAmino.UnmarshalJSON(resBytes, validator)
+	if err != nil {
+		log.WithError(err).Error("UnmarshalJSON")
+	}
+	return
+}
+
+/**
+
+*/
+func (this *TxClient) FindDelegation(delegatorAddr, validatorAddr string) (delegation *stakingTypes.DelegationResponse, notFound bool, err error) {
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
+	notFound = false
+	params := stakingTypes.QueryDelegatorValidatorRequest{DelegatorAddr: delegatorAddr, ValidatorAddr: validatorAddr}
+	bz, err := clientCtx.LegacyAmino.MarshalJSON(params)
+	if err != nil {
+		log.WithError(err).Error("MarshalJSON")
+		return nil, notFound, err
+	}
+	resBytes, _, err := clientCtx.QueryWithData("custom/staking/"+stakingTypes.QueryDelegation, bz)
+	if err != nil {
+		log.WithError(err).Error("QueryWithData")
+		if strings.Contains(err.Error(), stakingTypes.ErrNoDelegation.Error()) {
+			notFound = true
+		}
+		return nil, notFound, err
+	}
+	delegation = &stakingTypes.DelegationResponse{}
+	err = util.Json.Unmarshal(resBytes, delegation)
+	if err != nil {
+		log.WithError(err).Error("Unmarshal")
+	}
+	return
+}
+
+type ValidatorInfo struct {
+	stakingTypes.Validator
+	StartTime           int64  `json:"start_time"`
+	UnbondingTimeFormat string `json:"unbonding_time_format"`
+}
+
+/*
+（）
+@params status:
+
+	BOND_STATUS_UNSPECIFIED
+	BOND_STATUS_UNBONDED
+	BOND_STATUS_UNBONDING
+	BOND_STATUS_BONDED
+*/
+func (this *TxClient) QueryValidators(page, limit int, status string) ([]ValidatorInfo, error) {
+	log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
+
+	if status == "" {
+		status = stakingTypes.BondStatusBonded
+	}
+
+	params := stakingTypes.NewQueryValidatorsParams(page, limit, status)
+
+	bz, err := clientCtx.LegacyAmino.MarshalJSON(params)
+	if err != nil {
+		log.WithError(err).Error("clientCtx.LegacyAmino.MarshalJSON")
+		return nil, err
+	}
+
+	route := fmt.Sprintf("custom/%s/%s", stakingTypes.QuerierRoute, stakingTypes.QueryValidators)
+
+	res, _, err := clientCtx.QueryWithData(route, bz)
+	if err != nil {
+		log.WithError(err).Error("clientCtx.QueryWithData")
+		return nil, err
+	}
+
+	validatorsResp := make(stakingTypes.Validators, 0)
+	err = clientCtx.LegacyAmino.UnmarshalJSON(res, &validatorsResp)
+	if err != nil {
+		return nil, err
+	}
+
+	validatorInfos := make([]ValidatorInfo, 0)
+
+	for _, val := range validatorsResp {
+
+		valConsAddr, err := val.GetConsAddr()
+		if err != nil {
+			log.WithError(err).Error("GetConsAddr Err:" + err.Error())
+			return nil, err
+		}
+
+		
+		node, err := clientCtx.GetNode()
+		if err != nil {
+			log.WithError(err).Error("clientCtx.GetNode")
+			return nil, err
+		}
+		slashingParams := slashingTypes.QuerySigningInfoRequest{valConsAddr.String()}
+		bz, err := clientCtx.LegacyAmino.MarshalJSON(slashingParams)
+		if err != nil {
+			log.WithError(err).Error("MarshalJSON")
+			return nil, err
+		}
+
+		resBytes, _, err := clientCtx.QueryWithData("custom/slashing/"+slashingTypes.QuerySigningInfo, bz)
+		if err != nil {
+			log.WithError(err).Error("QueryWithData")
+			return nil, err
+		}
+		info := slashingTypes.ValidatorSigningInfo{}
+		err = clientCtx.LegacyAmino.UnmarshalJSON(resBytes, &info)
+		if err != nil {
+			log.WithError(err).Error("clientCtx.LegacyAmino.UnmarshalJSON(slashingTypes.ValidatorSigningInfo)")
+			return nil, err
+		}
+		if info.StartHeight == 0 {
+			info.StartHeight = 1
+		}
+		blockInfo, err := node.Block(context.Background(), &info.StartHeight)
+		if err != nil {
+			log.WithError(err).Error("node.Block")
+			return nil, err
+		}
+
+		
+		unbondingTimeFormat := ""
+		if val.UnbondingTime.Unix() != 0 {
+			unbondingTimeFormat = val.UnbondingTime.Format("2006-01-02 15:04")
+		}
+
+		ValidatorInfoEdit := ValidatorInfo{
+			val,
+			time.Now().Unix() - blockInfo.Block.Time.Unix(),
+			unbondingTimeFormat,
+		}
+
+		ValidatorInfoEdit.ConsensusPubkey = nil
+		validatorInfos = append(validatorInfos, ValidatorInfoEdit)
+
+	}
+
+	return validatorInfos, nil
+
+}
+
+type DecCoinsString struct {
+	Coins []struct {
+		Denom  string
+		Amount string
+	} `json:"coins"`
+}
+
+/**
+
+*/
+func (this *TxClient) QueryValCanWithdraw(accAddr string) (res distributionTypes.ValidatorAccumulatedCommission, err error) {
+
+	//log := core.BuildLog(core.GetStructFuncName(this), core.LmChainClient)
+
+	accFromAddress, err := sdk.AccAddressFromBech32(accAddr)
+	if err != nil {
+		return res, err
+	}
+	valAddr := sdk.ValAddress(accFromAddress)
+
+	// query commission
+	bz, err := common.QueryValidatorCommission(clientCtx, valAddr)
+	if err != nil {
+		return res, err
+	}
+
+	var commission distributionTypes.ValidatorAccumulatedCommission
+	clientCtx.LegacyAmino.UnmarshalJSON(bz, &commission)
+
+	return commission, nil
 }
