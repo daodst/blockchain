ad4dd6d99521a589fea92528b5ba61226050d4df
feischoll <141482965+feischolleimemo@users.noreply.github.com>
2024-05-08 14:55:48 +0800

    contract handler

--- x/contract/handler.go
+++ x/contract/handler.go
@@ -11,11 +11,17 @@ import (
 // NewHandler defines the erc20 module handler instance
 func NewHandler(k keeper.Keeper) sdk.Handler {
 
-	//msgServer := keeper.NewMsgServerImpl(k)
+	msgServer := keeper.NewMsgServerImpl(k)
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
+		case *types.MsgChatTokenIssue: 
+			res, err := msgServer.ChatTokenIssue(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgAppTokenIssue: 
+			res, err := msgServer.AppTokenIssue(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			err := sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, err
