660e794ef878af84e664ae3fda7dc19cdaf1fd46
Gervais Brochard <115452844+pamsquid@users.noreply.github.com>
2024-05-11 18:10:06 +0800

    genesis file

--- x/contract/genesis.go
+++ x/contract/genesis.go
@@ -1,11 +1,12 @@
 package contract
 
 import (
-	"fmt"
 	"freemasonry.cc/blockchain/contracts"
+	"freemasonry.cc/blockchain/core"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 
@@ -21,29 +22,89 @@ func InitGenesis(
 	data types.GenesisState,
 ) {
 	k.SetParams(ctx, data.Params)
+	//
+	initCoinMetadata(ctx, k)
 }
 
 // ExportGenesis export module status
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	return &types.GenesisState{
-		//Params:     k.GetParams(ctx),
-		//TokenPairs: k.GetTokenPairs(ctx),
+		Params: k.GetParams(ctx),
 	}
 }
 
-func ApplayNftContract(ctx sdk.Context, k keeper.Keeper, accountKeeper authkeeper.AccountKeeper) {
-	contractAddress := authtypes.NewModuleAddress(types.ModuleName)
+func RegisterCoin(ctx sdk.Context, k keeper.Keeper) {
+	//dst
+	//dstMetaData, found := k.BankKeeper.GetDenomMetaData(ctx, core.BaseDenom)
+	//if found {
+	//	_, err := k.RegisterCoin(ctx, dstMetaData)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	usdtMetaData, found := k.BankKeeper.GetDenomMetaData(ctx, core.UsdtDenom)
+	if found {
+		_, err := k.RegisterCoin(ctx, usdtMetaData)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func initCoinMetadata(ctx sdk.Context, k keeper.Keeper) {
+	coinMetadata := []banktypes.Metadata{
+		//{
+		//	Description: "dst coin",
+		//	Base:        core.BaseDenom,
+		//	DenomUnits: []*banktypes.DenomUnit{
+		//		{
+		//			Denom:    core.BaseDenom,
+		//			Exponent: 18,
+		//		},
+		//	},
+		//
+		//	Display: core.BaseDenom,
+		//	Name:    "cosmos dst",
+		//	Symbol:  "edst",
+		//},
+		{
+			Description: "usdt coin",
+			Base:        core.UsdtDenom,
+			DenomUnits: []*banktypes.DenomUnit{
+				{
+					Denom:    core.UsdtDenom,
+					Exponent: 18,
+				},
+			},
+			Display: core.UsdtDenom,
+			Name:    "bsc bridge usdt",
+			Symbol:  "usdt",
+		},
+	}
+	for _, metadatum := range coinMetadata {
+		k.BankKeeper.SetDenomMetaData(ctx, metadatum)
+	}
+}
+
+// 
+func DeployTokenFactoryContract(ctx sdk.Context, k keeper.Keeper) {
+	log := core.BuildLog(core.GetPackageFuncName(), core.LmChainXContract)
+	contractAddress := authtypes.NewModuleAddress(types.TokenFactoryContractDeploy)
 	from := common.BytesToAddress(contractAddress)
 	contractAddr := crypto.CreateAddress(from, 0)
-	fmt.Println("********************:", contractAddr)
-	freeMasonryMedal := contracts.FreeMasonryMedalJSONContract
-	resp, err := k.CallEVMWithData(ctx, from, nil, freeMasonryMedal.Bin, true)
+	log.Info("********************token factory contract address:", contractAddr)
+	StakeFactoryMedal := contracts.AppTokenIssueJSONContract
+	resp, err := k.CallEVMWithData(ctx, from, nil, StakeFactoryMedal.Bin, true)
 	if err != nil {
-		fmt.Println("************************:", err.Error())
+		log.WithError(err).Error("token factory contract deploy error")
 		panic(err)
 	}
-	fmt.Println("********************************:", resp.String())
-	err = k.SetNftContractAddress(ctx, contractAddr.String())
+	log.Info("********************************token factory contract deploy res:", resp.String())
+	err = k.SetTokenFactoryContractAddress(ctx, contractAddr.String())
+
+	conAddr := k.GetTokenFactoryContractAddress(ctx)
+	log.Info("*************************get token factory contract addr:", conAddr)
+
 	if err != nil {
 		panic(err)
 	}
